e10851b4880ec13e4e24c84b4fedc8918c9e904c
Robin De Moor <robindemoor04@gmail.com>
2025-05-08 01:31:11 +0300

    code directory

---
+++ code/GccApplication1/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 07/05/2025 19:22:44
+ * Author : robin
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <iostream>
+#include <delay.h>
+#define F_CPU 16000000UL
+
+void pcint_init(){
+	
+}
+
+
+int main(void){
+    while (1) {
+    }
+}
+
